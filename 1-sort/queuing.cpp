b2162606efcf652a4fe79b341d3645eea43c15c3
chaos-warzh <haidedeyu@163.com>
2024-09-22 01:08:12 +0800

    Fix bugs in queuing.cpp (Important!)

--- 1-sort/queuing.cpp
+++ 1-sort/queuing.cpp
@@ -2,34 +2,42 @@
 // Created by DELL on 2024/9/21.
 //
 
-
+// #2
+// very important! you must recite it !!!
 #include <cstdio>
 #include <vector>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int m, n;
 
-
 int main() {
-  scanf("%d%d", &m, &n);
-  vector<int> persons(100);
-  int index = 0; // of persons
+  scanf("%d%d ", &m, &n);
+  vector<int> persons(100 * 100 + 95);
+  int index = 0;
+
   for (int i = 0; i < m; i++) {
-    while (true) {
-      char ch;
-      scanf(" %c ", &ch);
-      if (ch != '#') {
-        persons[index] = ch - 48;
-        index++;
-      } else {
+    /* how to handle this kind of input -- A standard way */
+    string line;
+    getline(std::cin, line);
+
+    int num;
+    std::stringstream ss(line);
+
+    while (ss >> num) {
+      persons[index] = num;
+      index++;
+      ss >> std::ws;
+      if (ss.peek() == '#') { // or EOF
         break;
       }
     }
-    //
-    persons.erase(persons.end() - n + 1);
+
+    persons.erase(persons.begin() + index - n);
     index--;
-    for (const auto &item : persons) {
+    for (int i = 0; i < index; i++) {
+      const auto &item = persons[i];
       printf("%d ", item);
     }
     puts("#");
