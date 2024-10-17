9e0d6de2b67aa37d60f1ab4b3c6e8641dc1a3404
chaos-warzh <haidedeyu@163.com>
2024-10-18 00:13:23 +0800

    Refactor disjoint_intervals

--- cpp02/disjoint_intervals.cpp
+++ cpp02/disjoint_intervals.cpp
@@ -9,52 +9,40 @@
 
 using namespace std;
 
-bool cmp(vector<int>& a, vector<int>& b) {
-  return a[1] < b[1];
-}
-
-int minRem(vector<vector<int>> & durings) {
-  int n = durings.size();
+int minRem(vector<vector<int>>& intervals) {
+  int n = intervals.size();
   int r = 0;
   int last_end = INT_MIN;
   for (int i = 0; i < n; i++) {
-    if (durings[i][0] < last_end) {
+    if (intervals[i][0] < last_end) {
       ++r;
     } else {
-      last_end = durings[i][1];
+      last_end = intervals[i][1];
     }
   }
   return r;
 }
 
+/**
+ * 3
+ * 09:30-11:40
+ * 08:00-11:00
+ * 11:50-12:00
+ */
 int main() {
   int n;
   scanf("%d", &n);
-  vector<vector<int>>durings(n, vector<int>(2));
+  vector<vector<int>>intervals(n, vector<int>(2));
   for (int i = 0; i < n; i++) {
     char s[15];
     cin >> s;
-    durings[i][0] = ((s[0] - '0') * 10 + (s[1] - '0')) * 60 + (s[3] - '0') * 10 + (s[4] - '0');
-    durings[i][1] = ((s[6] - '0') * 10 + (s[7] - '0')) * 60 + (s[9] - '0') * 10 + (s[10] - '0');
+    intervals[i][0] = ((s[0] - '0') * 10 + (s[1] - '0')) * 60 + (s[3] - '0') * 10 + (s[4] - '0');
+    intervals[i][1] = ((s[6] - '0') * 10 + (s[7] - '0')) * 60 + (s[9] - '0') * 10 + (s[10] - '0');
   }
 
-  sort(durings.begin(), durings.end(), cmp);
-
-  vector<vector<int>> conflicts(n * n + n, vector<int> (2));
-
-  int confs = 0;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = i; j < n; j++) {
-      if (durings[i][1] > durings[j][0]) {
-        // conflicts
-        conflicts[confs][0] = i;
-        conflicts[confs][1] = j;
-        confs++;
-      }
-    }
-  }
+  sort(intervals.begin(), intervals.end(),
+                    [](vector<int>& a, vector<int>& b) { return a[1] < b[1]; } );
 
   // confs
-  printf("%d\n", minRem(durings));
+  printf("%d\n", minRem(intervals));
 }
