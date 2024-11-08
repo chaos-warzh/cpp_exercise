28a01342d4e190aae1ca237f7198f103c3b88b21
chaos-warzh <haidedeyu@163.com>
2024-11-08 23:59:47 +0800

    +merge (framework)

---
+++ 3-strings/merge.cpp
@@ -0,0 +1,23 @@
+//
+// Created by DELL on 2024/11/8.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string s, t, r;
+  cin >> s >> t >> r;
+
+  int lens = s.size();
+  int lent = t.size();
+  int lenr = r.size();
+  if (lens + lent != lenr) { puts("False"); return 0; }
+
+  int idxs = 0, idxt = 0, idxr = 0;
+  while (idxs < lens && idxt < lent) {
+
+  }
+
+  return 0;
+}
