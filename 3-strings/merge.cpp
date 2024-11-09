fe6e6aa1c526d3de181609807f1af0dd75c36502
chaos-warzh <haidedeyu@163.com>
2024-11-09 17:56:30 +0800

    +merge

--- 3-strings/merge.cpp
+++ 3-strings/merge.cpp
@@ -2,8 +2,29 @@
 // Created by DELL on 2024/11/8.
 //
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+// state compact array:
+vector<vector<bool>> been(100 + 1, vector<bool>(100 + 1, false));
+
+void merge(vector<vector<bool>> &dp, string &s, string &t, string &r, int s_idx, int t_idx) {
+  if (!been[s_idx][t_idx]) {
+    been[s_idx][t_idx] = true;
+  } else {
+    return;
+  }
+
+  dp[s_idx][t_idx] = true;
+  if (s_idx < dp.size() - 1 && s[s_idx] == r[s_idx + t_idx]) {
+    merge(dp, s, t, r, s_idx + 1, t_idx);
+  }
+
+  if (t_idx < dp[0].size() - 1 && t[t_idx] == r[s_idx + t_idx]) {
+    merge(dp, s, t, r, s_idx, t_idx + 1);
+  }
+}
 
 int main() {
   string s, t, r;
@@ -14,9 +35,15 @@ int main() {
   int lenr = r.size();
   if (lens + lent != lenr) { puts("False"); return 0; }
 
-  int idxs = 0, idxt = 0, idxr = 0;
-  while (idxs < lens && idxt < lent) {
+  // lens x lent
+  vector<vector<bool>> dp(lens + 1, vector<bool>(lent + 1, false));
 
+  merge(dp, s, t, r, 0, 0);
+
+  if (dp[lens][lent]) {
+    puts("True");
+  } else {
+    puts("False");
   }
 
   return 0;
